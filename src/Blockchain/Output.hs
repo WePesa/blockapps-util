301449200f8728ab60e2b750fbf9da5bd9a73025
Jamshid <qq@qq.com>
2016-05-23 23:16:52 -0700

    Cleanup some logging messages, and redirect logging messages to logs directory files for strato executable.

--- src/Blockchain/Output.hs
+++ src/Blockchain/Output.hs
@@ -3,6 +3,7 @@ module Blockchain.Output where
 
 import Control.Monad.Logger
 import qualified Data.ByteString.Char8 as BC
+import System.FilePath
 import System.GlobalLock
 import System.Log.FastLogger
 
@@ -10,4 +11,9 @@ printLogMsg::Loc->LogSource->LogLevel->LogStr->IO ()
 --printLogMsg loc logSource level msg = do
 printLogMsg _ _ _ msg = do
   lock $ putStrLn $ BC.unpack $ fromLogStr msg
+
+printToFile::FilePath->Loc->LogSource->LogLevel->LogStr->IO ()
+--printLogMsg loc logSource level msg = do
+printToFile path _ _ _ msg = do
+  lock $ appendFile path $ BC.unpack (fromLogStr msg) ++ "\n"
 
