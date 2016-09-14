82a9850481b38f269fa88ff9f80fd259ced1925e
Ryan Reich <ryan.reich@gmail.com>
2016-09-14 10:49:06 -0700

    Fix compilation

--- src/Blockchain/Format.hs
+++ src/Blockchain/Format.hs
@@ -10,6 +10,7 @@ import qualified Data.ByteString.Base16 as B16
 import qualified Data.NibbleString as N
 
 import Blockchain.ExtWord
+import Numeric
 
 class Format a where
   format::a->String
@@ -18,8 +19,8 @@ instance Format B.ByteString where
   format x = BC.unpack (B16.encode x)
 
 instance Format N.NibbleString where
-  format (EvenNibbleString bs) = format bs
-  format (OddNibleString n bs) = showHex n "" ++ format bs
+  format (N.EvenNibbleString bs) = format bs
+  format (N.OddNibbleString n bs) = showHex n "" ++ format bs
 
 instance Format Word256 where
   format x = BC.unpack $ B16.encode $ B.pack $ word256ToBytes x
